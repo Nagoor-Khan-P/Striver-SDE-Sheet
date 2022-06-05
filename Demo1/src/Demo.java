2b1b57a1b1c4d5567890e003a92c558549b8dfa3
Nagoor-Khan-P <106923967+Nagoor-Khan-P@users.noreply.github.com>
2022-06-05 22:39:07 +0530

    first commit

---
+++ Demo1/src/Demo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Demo {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		String name=scan.nextLine();
+		System.out.println("Yes! from today "+name+" is starting striver SDE sheet");
+		char ch=scan.next().charAt(0);
+		System.out.println("The first letter is "+ch);
+		
+	}
+}
